7810258225c05b36cce505f447bc40937b501b92
Kerby Jean <kerby.jean@hotmail.fr>
2020-04-24 13:43:19 -0400

    Added PokemonViewController

---
+++ Pokedex/PokemonViewController.h
@@ -0,0 +1,17 @@
+//
+//  PokemonViewController.h
+//  Pokedex
+//
+//  Created by Kerby Jean on 4/24/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PokemonViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
