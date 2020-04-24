12ae155b9963e57cd7ade29d8c32dcce9546422f
Kerby Jean <kerby.jean@hotmail.fr>
2020-04-24 12:42:32 -0400

    Added POPokemon+NSJSONSerialization class

---
+++ Pokedex/POPokemon+NSJSONSerialization.h
@@ -0,0 +1,17 @@
+//
+//  POPokemon+NSJSONSerialization.h
+//  Pokedex
+//
+//  Created by Kerby Jean on 4/24/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface POPokemon_NSJSONSerialization : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pokedex/POPokemon+NSJSONSerialization.m
@@ -0,0 +1,13 @@
+//
+//  POPokemon+NSJSONSerialization.m
+//  Pokedex
+//
+//  Created by Kerby Jean on 4/24/20.
+//  Copyright © 2020 Kerby Jean. All rights reserved.
+//
+
+#import "POPokemon+NSJSONSerialization.h"
+
+@implementation POPokemon_NSJSONSerialization
+
+@end
